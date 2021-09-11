2bff20724c7b188ba990cc3e08aaae229fa05226
yasukawa <kazu050107@gmail.com>
2021-09-11 17:02:25 +0000

    Laravel 08 課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,5 +7,24 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    // Laravel 08 課題応用
+    public function add()   // addはaction
+    {
+        return view('admin.profile.create');  // ProfileController -> Viewのadmin.profile.createに返す
+    }
+
+    public function create()
+    {
+        return redirect('admin/profile/create');    // -> admin/profile/createに転送する
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
 }
