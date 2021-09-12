6af59a9303ce3b8a02607ba09cf9f9d9a7b3201c
yasukawa <kazu050107@gmail.com>
2021-09-12 17:10:16 +0000

    Laravel 10　課題

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,14 @@
+<!-- Laravel 10 -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name"Viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyProfile</title>
+    </head>
+    <body>
+        <h1>Myプロフィール編集画面</h1>
+    </body>
+</html>
