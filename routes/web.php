77af20d03f47608b0c14381d18d8992e1dd272f5
yasukawa <kazu050107@gmail.com>
2021-09-12 08:01:39 +0000

    Laravel 09 課題

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::get('/', function () {
 // URI = 名前または場所を識別する書き方のルールの総称（親玉）。URLやURNはURIで定められたルールに従って書かれたり使われたりする。
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');    //Laravel 09 課題4:応用
+    Route::get('profile/edit', 'Admin\ProfileController@edit');    //Laravel 09 課題4:応用
 });
 
 // Laravel 09 課題
-Route::get('XXX', 'XXX\AAAController@bbb');
+Route::get('bbb', 'XXX\AAAController@bbb');
